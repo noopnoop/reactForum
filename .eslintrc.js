32ee43625f059e726af85ac678004dd98ab57fef
noops <redtinfoilcrane@gmail.com>
2021-06-11 20:55:43 -0400

    eslint and setup

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2020': true,
+    'node': true,
+    'mocha': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 11,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true
+    }
+  },
+  'rules': {
+    'indent': [
+      'warn',
+      2
+    ],
+    'linebreak-style': [
+      'warn',
+      'unix'
+    ],
+    'no-trailing-spaces': [
+      'warn'
+    ],
+    'no-unused-vars': [
+      'warn',
+      {
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_'
+      }
+    ],
+    'quotes': [
+      'warn',
+      'single',
+      'avoid-escape'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function Test() {
+  return (
+    <h1> Howdy </h1>
+  );
+}
+
+render (<Test />, document.getElementById('root'));
